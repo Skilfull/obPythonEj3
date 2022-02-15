859c1864dbd844a0997a4c4755385bd688e304b0
Marino Guardia Suárez <marinoguardiasuarez@gmail.com>
2022-02-15 01:31:31 +0100

    Primer commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# - Nombre
+# - Apellidos
+# - Edad
+# - Email
+# - Teléfono
+# - Casado (verdadero o falso)
+# - Con Hijos (verdadero o falso)
+# - Lista de amigos
+# - Películas vistas (diccionario con clave y valor. El valor será el título de la película)
+
+
+nombre = "Marino"
+apellidos = "Guardia Suárez"
+edad = 38
+email = "marinoguardiasuarez@gmail.com"
+telefono = 661842596
+casado = True
+conHijos = True
+listaAmigos = ["Adri", "Pablo", "Roberto", "Alfonso", "Pedro"]
+peliculasVistas = { "Pelicula 1": "Terminator", "Pelicula 2" : "El gran Patron", "Pelicula 3": "Titanic"}
+
+print(nombre, apellidos)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(conHijos)
+print(listaAmigos)
+print(peliculasVistas)
